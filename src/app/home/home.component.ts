0bcaa639b983464a34dcb5b718c055f17ca0a9b5
Francis <51071976+munch7@users.noreply.github.com>
2024-12-03 12:41:31 +0300

    pending carousel installation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { NgFor } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import {
+  CarouselComponent,
+  CarouselControlComponent,
+  CarouselInnerComponent,
+  CarouselItemComponent,
+  ThemeDirective
+} from '@coreui/angular';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    RouterModule
+    RouterModule,
+    ThemeDirective,
+    CarouselComponent,
+    CarouselInnerComponent,
+    NgFor,
+    CarouselItemComponent,
+    CarouselControlComponent,
+    RouterLink
   ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent  implements OnInit {
   services = [
     { name: 'Electronics', description: 'Indoor & outdoor repairs, Portable devices repairs' },
     { name: 'Plumbing', description: 'Any and all plumbing services' },
@@ -24,7 +40,23 @@ export class HomeComponent {
     { name: 'Home Repairs', description: 'Roofing, Masonry repairs, Carpentry' }
   ];
 
+  slides: any[] = new Array(3).fill({ 
+    id: -1, src: '', title: '', subtitle: '' 
+  });
+
   constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.slides[0] = {
+      src: './assets/img/angular.jpg'
+    };
+    this.slides[1] = {
+      src: './assets/img/react.jpg'
+    };
+    this.slides[2] = {
+      src: './assets/img/vue.jpg'
+    };
+  }
 
   navigateToService(service: string) {
     this.router.navigate(['/services', service]);
