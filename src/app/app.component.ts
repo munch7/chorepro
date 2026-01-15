6c6218a092cabb54b9f9b2edc3d6c59cd44aa7fc
Francis <51071976+munch7@users.noreply.github.com>
2026-01-15 17:00:36 +0300

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterModule
+    RouterModule,
+    CommonModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'chorepro';
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
 }
